1083bbd0630a68d3329f686a97dbf9768c097de2
Yue Yu <yue.yu.aka.fish@outlook.com>
2022-06-01 18:08:31 +0100

    update doxygen comments

--- include/rfat/types.h
+++ include/rfat/types.h
@@ -19,44 +19,72 @@ extern "C" {
 
 /**
  * @brief rfat_file_alloc is the item type of a file allocation table. Field
- * "next" points to the next item, except it equals to RFAT_UNUSED or RFAT_EOF.
+ * "next" points to the next item, except it equals to RFAT_UNUSED, RFAT_EOF, or
+ * RFAT_RESERVED.
  */
 typedef struct rfat_file_alloc {
-  int32_t next;
+  int32_t next; // index of next item or control value
 } __attribute__((__packed__)) rfat_file_alloc_t;
 
+/**
+ * @brief rfat_file_entry describes a file entry. The "name" field is quite the
+ * literal meaning, and it cannot be longer than 9 characters(ends with '\0'),
+ * entry_idx is the first FAT item index of this file. Details of other fields,
+ * "used" and "type", are described below.
+ */
 typedef struct rfat_file_entry {
-  uint8_t name[10];
-  uint8_t used : 1;
-  uint8_t type : 3;
-  uint8_t reserved0 : 4;
-  uint8_t reserved1;
-  uint32_t entry_idx;
+  uint8_t name[10];      // file name, ends with '\0'
+  uint8_t used : 1;      // 1: used, 0: unused
+  uint8_t type : 3;      // 0: image, 1: patch, 2: other file
+  uint8_t reserved0 : 4; // reserved
+  uint8_t reserved1;     // reserved
+  uint32_t entry_idx;    // index of the entry in the file allocation table
 } __attribute__((__packed__)) rfat_file_entry_t;
 
+/**
+ * @brief rfat_metadata_block is the type of a metadata block. It is always
+ * placed at the end of rFAT file system(so called reversed FAT). The first 32
+ * bits(4 bytes) is the magic number, then follows the block count which in type
+ * of uint32_t.
+ */
 typedef struct rfat_metadata_block {
-  // 16 bytes used
-  uint32_t magic;
-  uint32_t block_cnt;
+  uint32_t magic;        // magic number, must be "rFat"(0x54414672)
+  uint32_t block_cnt;    // block count
   int32_t fat_entry_idx; // file allocation table start index
   int32_t fet_entry_idx; // file entry table start index
-
-  // 16 bytes unused
-  uint8_t reserved[16];
+  uint8_t reserved[16];  // reserved for future use
 } __attribute__((__packed__)) rfat_metadata_block_t;
 
+/**
+ * @brief rfat_fat_block is the type of a file allocation table block. Each item
+ * describes the next block of current item or the control values like RFAT_EOF,
+ * RFAT_RESERVED, and RFA_UNUSED. For example, if block[0].next = 2, block[2] is
+ * the next block of block[0].
+ */
 typedef struct rfat_fat_block {
-  rfat_file_alloc_t data[RFAT_FAT_BLOCK_SZ];
+  rfat_file_alloc_t data[RFAT_FAT_BLOCK_SZ]; // FAT items
 } __attribute__((__packed__)) rfat_fat_block_t;
 
+/**
+ * @brief rfat_fet_block is the type of a file entry table block. Each item
+ * gives a file entry and its entry index. Goto rfat_file_entry_t description
+ * for more details.
+ */
 typedef struct rfat_fet_block {
-  rfat_file_entry_t data[RFAT_FET_BLOCK_SZ];
+  rfat_file_entry_t data[RFAT_FET_BLOCK_SZ]; // FET items
 } __attribute__((__packed__)) rfat_fet_block_t;
 
+/**
+ * @brief rfat_data_block is the type of a data block. It saves data in raw
+ * format.
+ */
 typedef struct rfat_data_block {
-  uint8_t data[RFAT_DATA_BLOCK_SZ];
+  uint8_t data[RFAT_DATA_BLOCK_SZ]; // raw data
 } __attribute__((__packed__)) rfat_data_block_t;
 
+/**
+ * @brief rfat_any_block is a union of all block types decleared above.
+ */
 typedef struct rfat_any_block {
   union {
     rfat_metadata_block_t metadata;
--- src/backend/backend.h
+++ src/backend/backend.h
@@ -10,18 +10,55 @@
 extern "C" {
 #endif // __cplusplus
 
-struct fs_area;
-
+/**
+ * @brief open a rFAT file system on a implementation defined media.
+ *
+ * @param id[in] the id of the media, the type of id is implementation defined.
+ * @param fap[out] the file access pointer of the opened file system, the
+ * content is implementation defined.
+ * @return [int] 0: success, -1: failure
+ */
 int fs_area_open(const void *id, struct fs_area **fap);
 
+/**
+ * @brief close a rFAT file system.
+ *
+ * @param fap[in] the file access pointer of the opened file system.
+ * @return [int] 0: success, -1: failure
+ */
 int fs_area_close(const struct fs_area *fap);
 
+/**
+ * @brief read a block of raw data from the opened file system.
+ *
+ * @param fap[in] the file access pointer of the opened file system.
+ * @param offset[in] the offset of the filesystem area to read.
+ * @param dst[out] the destination buffer to store the read data.
+ * @param len[in] the length of the data to read.
+ * @return [int] 0: success, -1: failure
+ */
 int fs_area_read(const struct fs_area *fap, size_t offset, void *dst,
                  size_t len);
 
+/**
+ * @brief write a block of raw data to the opened file system.
+ *
+ * @param fap[in] the file access pointer of the opened file system.
+ * @param offset[in] the offset of the filesystem area to write.
+ * @param src[in] the source buffer to write.
+ * @param len[in] the length of the data to write.
+ * @return [int] 0: success, -1: failure
+ */
 int fs_area_write(const struct fs_area *fap, size_t offset, const void *src,
                   size_t len);
 
+/**
+ * @brief get the availalbe size of the opened file system.
+ *
+ * @param fap[in] the file access pointer of the opened file system.
+ * @param sz[out] the available size of the opened file system.
+ * @return [int] 0: success, -1: failure
+ */
 int fs_area_get_size(const struct fs_area *fap, size_t *sz);
 
 #ifdef __cplusplus
